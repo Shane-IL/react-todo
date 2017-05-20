07b067039bb490b865f39aaae9e6a3f0e9393356
Shane Goodson <shane@webzai.com>
2017-05-20 17:32:29 +0300

    added todo list and item components and basic rendering of items to list

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -1,18 +1,36 @@
 import React from 'react';
+import TodoList from 'TodoList';
 
 export default class TodoApp extends React.Component{
     constructor(props) {
         super(props);
         this.state = {
-            count: 0,
-            countdownStatus: 'stopped'
+            todos: [
+                {
+                    id: 1,
+                    text: 'Walk the dinosaur'
+                },
+                {
+                    id: 2,
+                    text: 'Clean the spaceship'
+                },
+                {
+                    id: 3,
+                    text: 'Drink whisky'
+                },
+                {
+                    id: 4,
+                    text: 'Nap'
+                }
+            ]
         };
     }
 
     render(){
+        const {todos} = this.state;
         return(
             <div>
-                TodoApp.jsx
+                <TodoList todos={todos}/>
             </div>
         )
     }
